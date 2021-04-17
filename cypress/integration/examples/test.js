f96a0dfd9869d0d88f8c0df791c657f53355ae86
Kevin Hernandez <khkevinhernandez@gmail.com>
2021-04-16 22:20:02 -0600

    Work with each loop

--- cypress/integration/examples/test.js
+++ cypress/integration/examples/test.js
@@ -5,7 +5,29 @@ describe('My First Test Suite', () => {
       cy.visit('https://rahulshettyacademy.com/seleniumPractise');
       cy.get('.search-keyword').type('ca');
       cy.wait(2000);
+
       cy.get('.product:visible').should('have.length', 4);
+
+      // .find search only for .product child elements of .products
+      // search elements within .products -> which is the target parent
+      cy.get('.products').find('.product').should('have.length', 4);
+
+      cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click();
+
+      cy.get('.products').find('.product').each(($el, index, $list) => {
+
+        // selector to find the product name
+        //$el is the current element on the loop
+        const productName = $el.find('h4.product-name').text();
+        // $el is a wrapped jQuery element
+        if (productName.includes('Cashews')) {
+          // wrap this element so we can
+          // use cypress commands on it
+          $el.find('button').click();
+        } else {
+          // do something else
+        }
+      }) //end of each
 
       //expect(true).to.equal(true)
     })
