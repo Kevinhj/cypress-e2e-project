d78cd0270a753ded82693a48a9a95aba07c67b2f
Kevin Hernandez <khkevinhernandez@gmail.com>
2021-04-20 22:34:59 -0600

    work with radio button

--- cypress/integration/examples/section_6_tests.js
+++ cypress/integration/examples/section_6_tests.js
@@ -10,12 +10,10 @@ describe('Working with UI elements', () => {
     })
 
     it('Static dropdown test', () => {
-      //cy.visit('https://rahulshettyacademy.com/AutomationPractice/');
       cy.get('select').select('option2').should('have.value', 'option2');
     })
 
     it('Dynamic dropdown test', () => {
-      //cy.visit('https://rahulshettyacademy.com/AutomationPractice/');
       cy.get('#autocomplete').type('ind');
       cy.get('.ui-menu-item div').each(($el, index, $list) => {
         if($el.text() === 'India'){
@@ -24,4 +22,17 @@ describe('Working with UI elements', () => {
       })
       cy.get('#autocomplete').should('have.value', 'India');
     })
+
+    it('Visible and Invisible element test', () => {
+      cy.get('#displayed-text').should('be.visible');
+      cy.get('#hide-textbox').click();
+      cy.get('#displayed-text').should('not.be.visible');
+      cy.get('#show-textbox').click();
+      cy.get('#displayed-text').should('be.visible');
+    })
+
+    it('Radio button tests', () => {
+      cy.get('[value="radio2"]').check().should('be.checked');
+    })
+
   })
